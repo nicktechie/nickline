31ab73be070503f4fe0d364001c12ae4185f5919
Nick <nickomuyonga2@gmail.com>
2020-05-02 15:44:45 +0300

    second commit

---
+++ cv/index.php
@@ -0,0 +1,356 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+	<title>CV - NICKLINE OMUYONGA</title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="description" content="Nickline Omuyonga Online CV and Portfolio.">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="UTF-8">
+
+
+	<!-- Font -->
+
+
+
+	<!-- Stylesheets -->
+	<link href="style.css" rel="stylesheet" >
+
+
+	<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5dea5c17d96992700fcb1788/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+
+</head>
+<body>
+
+	<header>
+		<div class="container">
+			<div class="heading-wrapper">
+				<div class="row">
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-location-outline"></i>
+							<div class="right-area">
+								<h5>Kakamega</h5>
+								<h5>Kenya</h5>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-telephone-outline"></i>
+							<div class="right-area">
+								<h5> <a href="tel: + 254708744739"> +254708 744 739 </a> </h5>
+								<h6>Call, Sms, WhatsApp</h6>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+
+					<div class="col-sm-6 col-md-6 col-lg-4">
+						<div class="info">
+							<i class="icon ion-ios-chatboxes-outline"></i>
+							<div class="right-area">
+								<h5> <a href="mailto:nicklin@gmail.com"> mail@nickline.info </a> </h5>
+								<h6>Email me</h6>
+							</div><!-- right-area -->
+						</div><!-- info -->
+					</div><!-- col-sm-4 -->
+				</div><!-- row -->
+			</div><!-- heading-wrapper -->
+
+			<a class="downlad-btn" target="self" href="images/NICKLINE CV.pdf">Download CV</a>
+		</div><!-- container -->
+	</header>
+
+	<section class="intro-section">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-1 col-lg-2"></div>
+				<div class="col-md-10 col-lg-8">
+					<div class="intro">
+						<div class="profile-img"><img src="images/hey.jpg" alt=""></div>
+						<h2><b>Nickline Omuyonga</b></h2>
+						<h4 class="font-yellow">Computer Scientist</h4>
+						<ul class="information margin-tb-30">
+							<p>Grounded and solution-oriented Computer Scientist adept at learning, doing and motivating self and others. Passionate about learning and educating the next generation of technology users and innovators.</p>
+						</ul>
+						<ul class="social-icons">
+							<li><a href="https://github.com/nicktechie"><i class="ion-social-github"></i></a></li>
+							<li><a href="https://www.linkedin.com/in/nickline-omuyonga-8b304618a/"><i class="ion-social-linkedin"></i></a></li>
+							<li><a href="https://www.instagram.com/nickline_techie/"><i class="ion-social-instagram"></i></a></li>
+							<li><a href="https://www.facebook.com/nickline.omuyonga"><i class="ion-social-facebook"></i></a></li>
+							<li><a href="https://twitter.com/lin_nickline"><i class="ion-social-twitter"></i></a></li>
+						</ul>
+					</div><!-- intro -->
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- intro-section -->
+	<hr>
+	<!-- About me -->
+
+	<section class="about-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>About me</b></h3>
+						<h6 class="font-lite-black"><b>TECHNICAL IT SKILLS</b></h6>
+					</div>
+				</div><!-- col-sm-4 -->
+				<div class="col-sm-8">
+					<p class="margin-b-50">•  3+ years’ experience and good understanding of front-end technologies, including HTML5, CSS3, JavaScript, React Js, JQuery, bootstrap.</p>
+				   <p class="margin-b-50">•  2+ years’ experience Backend Web development: PHP, Node.js, Django, MySQL databases </p>
+				   <p class="margin-b-50"> • Passionate about programming: C++, Python, Java</p>
+				    <p class="margin-b-50"> • Android Developer with Kotlin</p>
+				  <p class="margin-b-50"> •  Troubleshooting and fixing common computer hardware, Windows software and network related problems</p>
+				  <p class="margin-b-50"> •  Proficiency in Microsoft Office suite (Word, Excel, Access, PowerPoint, Publisher) </p>
+				  <p class="margin-b-50"> •  Graphics design (MS Powerpoint, Photoshop, Blender) </p>
+				  <p class="margin-b-50"> •  Video Editing skills (Adobe Premier Pro, Adobe After Effects, Wondershare Filmora) </p>
+				  <p class="margin-b-50"> •  Passionate about Internet of Things (Arduino)</p>
+				  <p class="margin-b-50"> •  Data Analysis (Ms Excel)</p>
+				  <p class="margin-b-50"> •  Understanding of web hosting, Cpanel and basic server administration.</p>
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+	</section><!-- about-section -->
+		<hr>
+
+	<section class="portfolio-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>Portfolio</b></h3>
+						<h6 class="font-lite-black"><b>MY WORK</b></h6>
+					</div>
+				</div><!-- col-sm-4 -->
+				<div class="col-sm-8">
+
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+
+		<div class="portfolioContainer">
+
+			<div class="p-item web-design" >
+				<a href="#" data-fluidbox>
+					<img src="images/portfolio-4-600x400.jpg" alt=""></a>
+			</div><!-- p-item -->
+
+			<div class="p-item web-design">
+				<a href="#" data-fluidbox>
+					<img src="images/photo.jpg" alt=""></a>
+			</div><!-- p-item -->
+
+			<div class="p-item graphic-design web-design" >
+				<a href="#" data-fluidbox>
+					<img src="images/photo-153.jpg"  alt=""></a>
+			</div><!-- p-item -->
+
+		</div><!-- portfolioContainer -->
+
+	</section><!-- portfolio-section -->
+
+
+	<section class="experience-section section mt-5" >
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>Career Experience</b></h3>
+						<h6 class="font-lite-black"><b>PREVIOUS JOBS</b></h6>
+					</div>
+				</div><!-- col-sm-4 -->
+				<div class="col-sm-8">
+
+					<div class="experience margin-b-50">
+						<h4><b>FULLSTACK DEVELOPER</b></h4>
+						<h5 class="font-yellow"><b>MOI GIRLS VOKOLI</b></h5>
+						<h6 class="margin-t-10"> December 2019 – current</h6>
+						<p class="font-semi-white text-white margin-tb-30"> Am currently working for Moi Girls Vokoli as their web developer for site updates, optimizing their online presence and automating posting of content at the front end. Am also building their Student Management Information System.
+  </p>
+
+					</div><!-- experience -->
+
+
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+
+	</section><!-- experience-section -->
+
+	<section class="education-section section">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-4">
+					<div class="heading">
+						<h3><b>Education</b></h3>
+						<h6 class="font-lite-black"><b>ACADEMIC CAREER</b></h6>
+					</div>
+				</div><!-- col-sm-4 -->
+				<div class="col-sm-8">
+					<div class="education-wrapper">
+						<div class="education margin-b-50">
+							<h4><b>BACHELORS DEGREE IN COMPUTER SCIENCE</b></h4>
+							<h5 class="font-yellow"><b>Moi University</b></h5>
+							<h6 class="font-lite-black margin-t-10">2017 SEPTEMBER TO PRESENT</h6>
+							<p class="margin-tb-30">Knowledge in  Procedural programming, Object Oriented Programming, Systems hardware and software, Electronics, Statistics, Operating systems, Software engineering, Algorithms, Database systems, Networking, Information systems security, Data structures and Communication skills. </p>
+						</div><!-- education -->
+
+						<div class="education margin-b-50">
+							<h4><b>KENYA CERTIFICATE OF SECONDARY EDUCATION</b></h4>
+							<h5 class="font-yellow"><b>Maseno School</b></h5>
+							<h6 class="font-lite-black margin-t-10">COMPLETED IN NOVEMBER 2016</h6>
+							<p class="margin-tb-30">Scored 76 points/84 points (A- minus grade). </p>
+						</div><!-- education -->
+
+						<div class="education margin-b-50">
+							<h4><b>KENYA CERTIFICATE OF PRIMARY EDUCATION</b></h4>
+							<h5 class="font-yellow"><b>Vokoli Primary School</b></h5>
+							<h6 class="font-lite-black margin-t-10">COMPLETED IN NOVEMBER 2012</h6>
+							<p class="margin-tb-30">Scored 388 marks/ 500 marks </p>
+						</div><!-- education -->
+					</div><!-- education-wrapper -->
+				</div><!-- col-sm-8 -->
+			</div><!-- row -->
+		</div><!-- container -->
+
+	</section><!-- about-section -->
+	<hr>
+
+	<section class="accordian-section section">
+			<div class="container">
+
+				<div class="row">
+					<div class="col-sm-12">
+						<div class="heading">
+							<h4><b>More</b></h4>
+						</div>
+					</div><!-- col-sm-12 -->
+
+
+					<div class="col-md-12 ">
+						<div class="panel margin-b-30">
+
+
+							<div class="panel-area margin-b-30">
+								<h5 class="panel-title"><b>HOBIES</b>
+									<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+								<p class="panel-desc">
+									•	Video editing<br>
+									•	Browsing<br>
+									•	Blogging<br>
+									•	Playing computer games<br>
+									•	Photography<br>
+									•	Nature walks<br>
+								</p>
+							</div><!-- panel-area -->
+
+							<div class="panel-area margin-b-30">
+								<h5 class="panel-title"><b>REFEREES</b>
+									<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+								<p class="panel-desc">
+									<b>• Mr Orango Vitalis</b><br>
+									Data Manager, Moi Teaching & Referal Hospital<br>
+									Phone: 0720438875<br>
+									<a href="">vitaliorango@gmail.com</a><br><br>
+
+									<b>• Patricia Izava</b><br>
+									Secretary, Moi Girls Vokoli<br>
+									Phone: 0720605698<br>
+									<a href="">patriciaizava@gmail.com</a><br><br>
+								</p>
+							</div><!-- panel-area -->
+
+							<div class="panel-area margin-b-30">
+								<h5 class="panel-title"><b>CAREER EVENTS AND FORUMS</b>
+									<i class="icon minus ion-minus"></i><i class="icon plus ion-plus"></i></h5>
+								<p class="panel-desc">
+
+									•	<b>Technology Students Association(TSA) Career Week
+									</b><br> <i> March 12th, 2020 - Moi University </i><br>
+
+									-Attended as a student;
+									Learnt CV writing, self branding, networking and career opportunities offered by various companies. <br><br>
+
+
+
+									•	<b>Institute of Electrical and Electronics Engineers(IEEE)
+									</b><br> <i> February 26th, 2020 - Moi University </i><br>
+
+									-Attended as an Android Developer;
+									Learning Machine Learninng in Android by use of Google Firebase <br><br>
+
+									•	<b>Myinfotech - Education and E-learning Security event
+									</b><br> <i> November 15th, 2019 - Online Event </i><br>
+
+									-Attended as a student on the topics; SSH, Injection flaws, Session hijacking etc. to identify vulnerabilities and attacks <br><br>
+
+									
+
+									•	<b>Facebook Developer Circles Eldoret - Meetup
+									</b><br> <i> October 27th, 2019 - Eldoret Kenya </i><br>
+
+									-Attended as a developer on the topics; Accessibility of applications, Developer communities, Importance of testing, Internet of Things ,Mentorship, Git/GitHub <br><br>
+
+									•	<b>Code Week by ASAP SE
+									</b><br> <i>April 13th, 2019 - Vokoli Kenya </i><br>
+									-Attended as an innovator on the theme: The role of innovation in national development  <br><br>
+								</p>
+
+							</div><!-- panel-area -->
+						</div><!-- panel -->
+					</div><!-- col-sm-6-->
+
+				</div><!-- row -->
+			</div><!-- container -->
+		</section><!-- accordian-section -->
+
+	<section class="counter-section" id="counter">
+		<div class="container">
+			<div class="row">
+
+		</div><!-- container-->
+    </section><!-- counter-section-->
+
+	<footer>
+		<p class="copyright">
+
+Copyright &copy;<script>document.write(new Date().getFullYear());</script>
+
+		</p>
+
+
+	</footer>
+
+
+	<!-- SCIPTS -->
+
+	<script src="common-js/jquery-3.2.1.min.js"></script>
+
+
+	<script src="common-js/bootstrap.js"></script>
+
+	<script src="common-js/isotope.pkgd.min.js"></script>
+
+	<script src="common-js/jquery.waypoints.min.js"></script>
+
+
+	<script src="common-js/jquery.fluidbox.min.js"></script>
+
+	<script src="common-js/scripts.js"></script>
+
+</body>
+</html>
